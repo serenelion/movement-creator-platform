f2dbeb322211651d93f5235a2e27f5c072bc3cda
serenelion <124634508+serenelion@users.noreply.github.com>
2024-12-04 23:22:19 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -58,13 +58,13 @@ export default async function middleware(req: NextRequest) {
     );
   }
 
-  // rewrite root application to `/home` folder
+  // rewrite root application to `/app` folder
   if (
     hostname === "localhost:3000" ||
     hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
   ) {
     return NextResponse.rewrite(
-      new URL(`/home${path === "/" ? "" : path}`, req.url),
+      new URL(`/app{path === "/" ? "" : path}`, req.url),
     );
   }
 
